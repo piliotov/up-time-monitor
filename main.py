73aa59fc854d94608776c79cf6ef69d263f0c891
Ilia Panayotov <76968111+piliotov@users.noreply.github.com>
2025-12-14 00:41:11 +0200

    Single check of a website can be performed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from datetime import datetime
-import sqlite3, json
+from datetime import datetime, timezone
+import time
+import sqlite3
+import json
+import httpx
 
 with open("config.json") as config_file:
     config = json.load(config_file)
@@ -7,7 +10,7 @@ with open("config.json") as config_file:
 url = config["url"]
 check_interval = config["interval_sec"]
 check_timeout = config["timeout_sec"]
-latency = config["max_latency_ms"]
+latency_max = config["max_latency_ms"]
 
 db = sqlite3.connect("uptime_monitor.db")
 c = db.cursor()
@@ -22,3 +25,32 @@ c.execute("""
           )
           """)
 db.commit()
+
+
+def check_website():
+    current_time = datetime.now(timezone.utc).isoformat()
+    try:
+        time_start = time.time()
+        response = httpx.get(url, timeout=check_timeout)
+        latency_ms = int((time.time() - time_start) * 1000)
+        status_code = response.status_code
+        ok = int(200 <= status_code < 400 and latency_ms <= latency_max)
+        error_msg = None
+    except Exception as e:
+        status_code = None
+        ok = 0
+        latency_ms = None
+        error_msg = str(e)
+    db.execute("""
+        INSERT INTO checks (ts_utc, status_code, ok_state, latency_ms, error_msg)
+        VALUES (?, ?, ?, ?, ?)""", (current_time, status_code, ok, latency_ms, error_msg))
+    db.commit()
+
+
+def main():
+    check_website()
+    print("Uptime monitor ran a single check.")
+
+
+if __name__ == "__main__":
+    main()
